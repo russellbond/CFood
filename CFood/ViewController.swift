25db5d7550c6a20509d38c82966aea158cf2526d
Mac Pro <macpro@rb-mac-pro.local>
2018-03-24 09:28:40 -0700

    Completed CFood project using Watson. Returns first result in array as image label

--- CFood/ViewController.swift
+++ CFood/ViewController.swift
@@ -7,19 +7,107 @@
 //
 
 import UIKit
+import VisualRecognitionV3
+import SVProgressHUD
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let apiKey = "80d442de211aba25966f4dbe4175068522c3d852"
+    let version = "2018-04-23"
+    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    var results: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        shareButton.isHidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        cameraButton.isEnabled = false
+        navigationItem.title = "calculating..."
+        SVProgressHUD.show()
+        
+        if let userImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = userImage
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visRec = VisualRecognition(apiKey: apiKey, version: version)
+            
+            let imageData = UIImageJPEGRepresentation(userImage, 0.01)
+            
+            let docURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let fileURL = docURL.appendingPathComponent("tempImage.jpeg")
+            
+            try? imageData?.write(to: fileURL, options: [])
+            
+            visRec.classify(imagesFile: fileURL, success: { (ClassifiedImages) in
+                let classes = ClassifiedImages.images.first!.classifiers.first!.classes
+                self.results = []
+                for index in 0..<classes.count {
+                    self.results.append(classes[index].className)
+                }
+                SVProgressHUD.dismiss()
+                print(self.results)
+                DispatchQueue.main.async {
+                    
+                    self.navigationItem.title = self.results.first!
+                    self.cameraButton.isEnabled = true
+                    self.shareButton.isHidden = false
+                }
+                
+                
+//                if self.results.contains("hotdog") {
+//                    DispatchQueue.main.async {
+//                        self.navigationItem.title = "Hot Dog!"
+//                    }
+//                } else {
+//                    DispatchQueue.main.async {
+//                        self.navigationItem.title = "NOT a Hotdog ;("
+//                    }
+//
+//                }
+            })
+            
+        }else{
+            print("ooops")
+        }
     }
-
-
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        navigationItem.title = ""
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func sharePressed(_ sender: UIButton) {
+        
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            vc?.setInitialText("Isn't this cool?!?")
+            vc?.add(imageView.image)
+            present(vc!, animated: true, completion: nil)
+        } else {
+            self.navigationItem.title = "Log in to Twitter"
+        }
+        
+    }
+    
 }
 
